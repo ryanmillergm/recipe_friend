194fe199572dec703190b185e48f7c1f59e2b84f
Ryan Miller <ryanmillergm@gmail.com>
2019-09-26 11:18:31 -0600

    Adds steps to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_171334) do
+ActiveRecord::Schema.define(version: 2019_09_26_171748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 2019_09_26_171334) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+  end
+
+  create_table "steps", force: :cascade do |t|
+    t.text "step"
   end
 
   create_table "users", force: :cascade do |t|
