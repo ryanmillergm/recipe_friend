c6b37d35261fec0c24c03495da055f95914f6e8f
Ryan Miller <ryanmillergm@gmail.com>
2019-09-26 17:35:41 -0600

    Removes save and open page

--- spec/features/visitors/visitor_welcome_page_spec.rb
+++ spec/features/visitors/visitor_welcome_page_spec.rb
@@ -33,8 +33,6 @@ RSpec.describe 'As a visitor' do
       click_on "Search Recipes"
 
       expect(current_path).to eq('/recipes')
-      save_and_open_page
-
 
       within(first('.recipe-results')) do
         expect(page).to have_css(".title")
