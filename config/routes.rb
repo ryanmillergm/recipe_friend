8c29f160f7d0a501f580ccc86c71fbf6fcb157fc
Ryan Miller <ryanmillergm@gmail.com>
2019-09-28 12:12:52 -0600

    Adds login index route

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/recipes', to: 'recipes#index'
 
-  get '/login', to: 'sessions#new'
-
-  
-
   resources :users, only: [:create, :new, :update, :edit]
-
+  resources :login, only: [:index]
 end
