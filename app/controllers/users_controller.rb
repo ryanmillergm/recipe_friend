e07fd27019850cf1f82e462b1e69cf15ca759975
Ryan Miller <ryanmillergm@gmail.com>
2019-09-28 11:41:30 -0600

    Adds create and user params methods for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,37 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      ready_messages
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name,
+                                 :last_name,
+                                 :username,
+                                 :password,
+                                 :password_confirmation,
+                                 :email,
+                                 :avatar,
+                                 :about,
+                                 :telephone)
+  end
+
+  def ready_messages
+
+  end
 end
