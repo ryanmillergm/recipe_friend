df8923294046cf848a085e62b2e9ab98cc4722f6
Ryan Miller <ryanmillergm@gmail.com>
2019-09-28 12:09:07 -0600

    Adds users routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/recipes', to: 'recipes#index'
+
+  get '/login', to: 'sessions#new'
+
+  
+
+  resources :users, only: [:create, :new, :update, :edit]
+
 end
