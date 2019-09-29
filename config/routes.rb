6b417874563f7f2c463c913019c9c5428482c1ba
Ryan Miller <ryanmillergm@gmail.com>
2019-09-29 13:34:36 -0600

    Adss destroy session route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users, only: [:create, :new, :update, :edit, :show]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :recipes, only: [:index]
 end
