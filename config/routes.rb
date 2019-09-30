9e63e07ca83900b0fcca5c19b87766819ba07d42
Ryan Miller <ryanmillergm@gmail.com>
2019-09-30 11:40:47 -0600

    Adds users confirm email to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :new, :update, :edit, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :recipes, only: [:index]
+
+  resources :users do
+    member do
+      get :confirm_email
+    end
+  end
 end
