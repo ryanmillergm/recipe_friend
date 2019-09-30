4613512b20a8c64486f26bf2456b5070749d2824
Ryan Miller <ryanmillergm@gmail.com>
2019-09-30 11:42:56 -0600

    Adds usermailer.registration confirmation to users create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.registration_confirmation(@user).deliver
       session[:user_id] = @user.id
       ready_messages
       redirect_to user_path(@user.id)
     else
+      flash[:error] = "Ooooppss, something went wrong!"
       render :new
     end
   end
@@ -38,6 +40,6 @@ class UsersController < ApplicationController
 
   def ready_messages
     flash[:message] = "#{@user.username}'s account has been created"
-    flash[:success] = 'This account has not yet been activated. Please check your email.'
+    flash[:success] = 'A confirmation email has been sent. To gain full access, please confirm your email. Thanks!'
   end
 end
