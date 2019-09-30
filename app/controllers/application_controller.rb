5a3f7fd4ee32444a048a92b9cc63f0014d68913e
Ryan Miller <ryanmillergm@gmail.com>
2019-09-30 11:42:22 -0600

    Adds confirm email method to session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def authorize
+    redirect_to new_session, alert: "Not authorized" if current_user.nil?
+  end
 end
