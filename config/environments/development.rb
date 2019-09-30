1909cdb990fcc6f5df5229d9ab25c1537964a95b
Ryan Miller <ryanmillergm@gmail.com>
2019-09-30 11:54:05 -0600

    Adds aws ses configuration to application.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,12 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -31,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
