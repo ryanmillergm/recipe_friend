6ac7c7aff633a59e77827c50bbc863b3f78b2af0
Ryan Miller <ryanmillergm@gmail.com>
2019-09-30 11:53:21 -0600

    Adds mailer configuration for aws ses in application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,3 +31,18 @@ module RecipeFriend
     config.generators.system_tests = nil
   end
 end
+
+class Application < Rails::Application
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    domain:               'example.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  config.generators.system_tests = nil
+end
