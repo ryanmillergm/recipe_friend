75f550efe6f8cad4fa3a37e9a1777e36c33f380b
Ryan Miller <ryanmillergm@gmail.com>
2019-10-04 09:42:54 -0600

    Adds user creates recipe spec

---
+++ spec/features/users/user_can_create_recipe_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  it 'I can add a new recipe' do
+
+    visit dashboard_path
+
+    click_on 'Add Recipe'
+
+    expect(current_path).to eq(new_recipe_path)
+
+    fill_in 'recipe[title]', with: "Spaghetti"
+    fill_in 'recipe[description]', with: "This is a recipe for spaghetti"
+    select 'pasta', from: :food_type_list
+
+    click_on 'Add Ingredients'
+
+    expect(current_path).to eq(new_ingredient_path)
+
+    fill_in 'ingredient[name]', with: "Spaghetti"
+    select 'spice', from: :ingredient_type_list
+
+    click_on 'Add Ingredient'
+
+    expect(current_path).to eq(edit_recipe_ingredients_path)
+
+    select 'volume/liquid', from: :measurement_type_list
+    select 'cup', from: :measurement_list
+    fill_in 'recipe_ingredient[description]', with: "This is a recipe for spaghetti"
+  end
+end
