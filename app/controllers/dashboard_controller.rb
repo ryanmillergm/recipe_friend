4d568c16605857d9cf4cd1e39bd3c4f956f1709a
Ryan Miller <ryanmillergm@gmail.com>
2019-10-04 16:55:57 -0600

    Adds current user must be register to access dashboard or redirected to login

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
   def index
-    
+    @user = current_user
+    if current_user.nil?
+      flash[:message] = 'You must log in or register to become a member'
+      redirect_to new_session_path
+    end
   end
 end
