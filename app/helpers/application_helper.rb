6f5846665f6b1058ce50fc8ce2302cb9356d58db
Ryan Miller <ryanmillergm@gmail.com>
2019-10-04 09:46:04 -0600

    Adds measurement and measurement type helpers to application helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def measurement_type
+    [
+      ['volume/liquid'],
+      ['weight'],
+      ['each'],
+    ]
+  end
+
+  def measurement
+    [
+      ['Fluid Ounce'],
+      ['Cup'],
+      ['Pint'],
+      ['Quart'],
+      ['Gallon'],
+    ]
+  end
 end
