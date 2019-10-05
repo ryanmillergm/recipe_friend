a50b0908adbbed2d4d1321ba0b76c89c5a5c7dd1
Ryan Miller <ryanmillergm@gmail.com>
2019-10-05 10:01:38 -0600

    Updates user creates recipe spec

--- spec/features/users/user_can_create_recipe_spec.rb
+++ spec/features/users/user_can_create_recipe_spec.rb
@@ -2,30 +2,47 @@ require 'rails_helper'
 
 RSpec.describe 'As a user' do
   it 'I can add a new recipe' do
+    user = create(:user)
 
-    visit dashboard_index_path
+    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    click_on 'Add Recipe'
+    visit root_path
 
-    expect(current_path).to eq(new_recipe_path)
+    click_on 'Log in'
+
+    expect(current_path).to eq(new_session_path)
+
+    fill_in 'session[email]', with: user.email
+    fill_in 'session[password]', with: user.password
+
+    click_on 'Log In'
+
+    expect(current_path).to eq(user_path(user.id))
+
+    click_on 'Dashboard'
+    click_button 'Add Recipe'
+
+    expect(current_path).to eq(new_user_recipe_path(user.id))
 
     fill_in 'recipe[title]', with: "Spaghetti"
     fill_in 'recipe[description]', with: "This is a recipe for spaghetti"
-    select 'American', from: :recipe_type_list
+    select 'American', from: :recipe_type
 
-    click_on 'Add Ingredients'
+    click_on 'Create Recipe'
 
     expect(current_path).to eq(new_ingredient_path)
 
     fill_in 'ingredient[name]', with: "Spaghetti"
-    select 'spice', from: :ingredient_type_list
+    select 'spice', from: :ingredient_type
+
+    select 'volume', from: :measurement_type
+    select 'cup', from: :measurement
+
+    fill_in 'ingredient[recipe_ingredients][quantity]', with: 1.5
 
     click_on 'Add Ingredient'
 
-    expect(current_path).to eq(edit_recipe_ingredients_path)
+    expect(current_path).to eq(new_ingredient_path)
 
-    select 'volume/liquid', from: :measurement_type_list
-    select 'cup', from: :measurement_list
-    fill_in 'recipe_ingredient[description]', with: "This is a recipe for spaghetti"
   end
 end
