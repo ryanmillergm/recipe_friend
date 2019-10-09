12b208bc3732d3c467a94296acea7932dd0ef782
Ryan Miller <ryanmillergm@gmail.com>
2019-10-09 11:29:16 -0600

    Adds favorites create method

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,14 @@ class FavoritesController < ApplicationController
   def index
     @user = current_user
     check_logged_in
-    
+  end
+
+  def create
+    @user = current_user
+    @recipe = Recipe.find(params[:format].to_i)
+    @favorite = Favorite.create(favorite_params(@user, @recipe))
+    flash[:message] = "#{@recipe.title} has been added to your favorites!"
+    redirect_to recipe_path(@recipe.id)
   end
 
   private
@@ -12,5 +19,12 @@ class FavoritesController < ApplicationController
       flash[:message] = 'You must log in or register to become a member'
       redirect_to new_session_path
     end
+  end
+
+  def favorite_params(user, recipe)
+    fav_params = {
+      user_id: user.id,
+      recipe_id: recipe.id
+    }
   end
 end
