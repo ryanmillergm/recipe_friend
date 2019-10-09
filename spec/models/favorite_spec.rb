0a23f8cdb09aed6a8e70db45b31fba91f56d5e6e
Ryan Miller <ryanmillergm@gmail.com>
2019-10-09 10:41:06 -0600

    Adds favorite model spec

---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Favorite, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :recipe_id }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :user }
+    it { should have_many :recipes }
+  end
+end
