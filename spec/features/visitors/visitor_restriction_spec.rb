b8d004ad274502595b73ad19f8676e153618a0b8
Ryan Miller <ryanmillergm@gmail.com>
2019-10-11 11:28:05 -0600

    Updates visitor restriction spec

--- spec/features/visitors/visitor_restriction_spec.rb
+++ spec/features/visitors/visitor_restriction_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
   it 'I cannot visit the dashboard' do
-    visit dashboard_index_path
+    visit dashboard_path
 
     expect(current_path).to eq(new_session_path)
     expect(page).to have_content('You must log in or register to become a member')
