b8de086bb805d23c688a88eb7a876d234d708954
Ryan Miller <ryanmillergm@gmail.com>
2019-10-12 16:06:23 -0600

    Adds has one attached image for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,6 +7,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   has_many :steps
+  has_one_attached :image
 
   def self.get_recipes(search_params)
     Recipe.where("title LIKE ?", "%#{search_params[:q].downcase.titleize}%").includes(:recipe_ingredients, :ingredients)
