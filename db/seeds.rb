a0c62f9a6916a7483b44c2fb82c072dfbebc8933
Ryan Miller <ryanmillergm@gmail.com>
2019-10-14 22:50:02 -0600

    Adds pot roast to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 @user1 = User.create(first_name: "ryan", last_name: "miller", username: "user1", password: "password", email: "user1@gmail.com", role: 1, avatar: "avatar", about: "Me", telephone: 5555555555)
 @user2 = User.create(first_name: "user", last_name: "user", username: "user2", password: "password", email: "user2@gmail.com", role: 0, avatar: "avatar", about: "Me", telephone: 5555555555)
@@ -23,18 +16,24 @@
 @ingredient12 = Ingredient.create(name: "tomato sauce", ingredient_type: 2)
 @ingredient13 = Ingredient.create(name: "spaghetti noodles", ingredient_type: 3)
 @ingredient14 = Ingredient.create(name: "egg", ingredient_type: 3)
+@ingredient15 = Ingredient.create(name: "beef chuck", ingredient_type: 3)
+@ingredient16 = Ingredient.create(name: "potatoes", ingredient_type: 5)
+@ingredient17 = Ingredient.create(name: "beef stock", ingredient_type: 2)
 
 @recipe1 = Recipe.create(title: "Spaghetti", description: "This recipe is for Spaghetti", recipe_type: 2, user_id: @user1.id)
 @recipe2 = Recipe.create(title: "Chicken Noodle Soup", description: "This recipe is for chicken noodle soup", recipe_type: 2, user_id: @user1.id)
 @recipe3 = Recipe.create(title: "Fresh Lasagna pasta", description: "This recipe is for lasagna noodles", recipe_type: 3, user_id: @user1.id)
+@recipe4 = Recipe.create(title: "Pot Roast", description: "This recipe is for a delicious slow cooked beef potroast", recipe_type: 3, user_id: @user1.id)
 
 @recipe1.image.attach(io: File.open('app/assets/images/Spaghetti-Meat-Sauce.jpg'), filename: 'Spaghetti-Meat-Sauce.jpg', content_type: "image/jpeg")
 @recipe2.image.attach(io: File.open('app/assets/images/chicken-noodle-soup.jpg'), filename: 'chicken-noodle-soup.jpg', content_type: "image/jpeg")
 @recipe3.image.attach(io: File.open('app/assets/images/homemade-pasta-lasagna-sheet.jpg'), filename: 'homemade-pasta-lasagna-sheet.jpg', content_type: "image/jpeg")
+@recipe4.image.attach(io: File.open('app/assets/images/Pot-Roast.jpg'), filename: 'Pot-Roast.jpg', content_type: "image/jpeg")
 
 @step1 = Step.create(step: "Add all ingredients together and mix", recipe_id: @recipe1.id)
 @step2 = Step.create(step: "Add all ingredients together and mix", recipe_id: @recipe2.id)
 @step3 = Step.create(step: "Add all ingredients together and mix", recipe_id: @recipe3.id)
+@step4 = Step.create(step: "Add all ingredients together and mix", recipe_id: @recipe4.id)
 
 @recipe_ingredient1 = RecipeIngredient.create(measurement_type: 1, measurement: 1, quantity: 2, ingredient_id: @ingredient1.id, recipe_id: @recipe1.id)
 @recipe_ingredient2 = RecipeIngredient.create(measurement_type: 0, measurement: 0, quantity: 2, ingredient_id: @ingredient2.id, recipe_id: @recipe1.id)
@@ -48,4 +47,8 @@
 @recipe_ingredient9 = RecipeIngredient.create(measurement_type: 2, measurement: 3, quantity: 2, ingredient_id: @ingredient11.id, recipe_id: @recipe2.id)
 
 @recipe_ingredient10 = RecipeIngredient.create(measurement_type: 2, measurement: 4, quantity: 2, ingredient_id: @ingredient4.id, recipe_id: @recipe3.id)
-@recipe_ingredient10 = RecipeIngredient.create(measurement_type: 2, measurement: 2, quantity: 2, ingredient_id: @ingredient14.id, recipe_id: @recipe3.id)
+@recipe_ingredient11 = RecipeIngredient.create(measurement_type: 2, measurement: 2, quantity: 2, ingredient_id: @ingredient14.id, recipe_id: @recipe3.id)
+
+@recipe_ingredient12 = RecipeIngredient.create(measurement_type: 2, measurement: 2, quantity: 2, ingredient_id: @ingredient15.id, recipe_id: @recipe4.id)
+@recipe_ingredient13 = RecipeIngredient.create(measurement_type: 1, measurement: 3, quantity: 3, ingredient_id: @ingredient16.id, recipe_id: @recipe4.id)
+@recipe_ingredient13 = RecipeIngredient.create(measurement_type: 1, measurement: 2, quantity: 4, ingredient_id: @ingredient17.id, recipe_id: @recipe4.id)
