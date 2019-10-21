ae977e8a2967aa1ad4c4b66c8ed3054438f85dfb
Ryan Miller <ryanmillergm@gmail.com>
2019-10-21 11:10:59 -0600

    Adds my recipes index

--- app/controllers/dashboard/my_recipes_controller.rb
+++ app/controllers/dashboard/my_recipes_controller.rb
@@ -1,8 +1,8 @@
-class MyRecipes::DashboardController < ApplicationController
+class Dashboard::MyRecipesController < ApplicationController
   def index
     if current_user
       @user = current_user
-      @recipes_facade = RecipeFacade.new(@user)
+      @my_recipes = Recipe.my_recipes(@user.id)
     else
       flash[:message] = 'You must log in or register to become a member'
       redirect_to new_session_path
