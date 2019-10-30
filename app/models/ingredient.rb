67627df63ebcc872d70a609c7128c085e6b8bfb8
Ryan Miller <ryanmillergm@gmail.com>
2019-10-30 13:53:57 -0600

    Adds dependent destroy to ingredient model on recipe ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,6 +4,6 @@ class Ingredient < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :ingredient_type
 
-  has_many :recipe_ingredients
+  has_many :recipe_ingredients, dependent: :destroy
   has_many :recipes, through: :recipe_ingredients
 end
