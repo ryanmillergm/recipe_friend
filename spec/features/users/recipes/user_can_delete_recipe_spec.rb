498c0ad4b61d96d6e098696e3b668251d1534fc4
Ryan Miller <ryanmillergm@gmail.com>
2019-10-30 15:40:54 -0600

    Updates user can delete recipe spec

--- spec/features/users/recipes/user_can_delete_recipe_spec.rb
+++ spec/features/users/recipes/user_can_delete_recipe_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe 'As a user' do
   before :each do
     @user1 = create(:user)
+    @user2 = create(:user)
     @ingredients1 = create(:ingredient)
     @recipe1 = create(:recipe, user_id: @user1.id)
     @ri1 = create(:recipe_ingredient, recipe_id: @recipe1.id, ingredient_id: @ingredients1.id)
     @recipe1.image.attach(io: File.open('app/assets/images/Spaghetti-Meat-Sauce.jpg'), filename: 'Spaghetti-Meat-Sauce.jpg', content_type: "image/jpeg")
   end
 
-  it 'I can update a recipe' do
+  it 'I can delete a recipe' do
     visit root_path
 
     click_on 'Log in'
@@ -29,9 +30,13 @@ RSpec.describe 'As a user' do
       click_on "#{@recipe1.title}"
     end
 
-    click_on 'Delete Recipe'
+    click_button 'Delete Recipe'
 
     expect(current_path).to eq(dashboard_my_recipes_path)
-    expect(page).to_not have_content(@recipe1.title)
+    expect(page).to have_content("You have successfully deleted #{@recipe1.title}")
+
+    within('.recipe-count') do
+      expect(page).to have_content("0 recipes")
+    end
   end
 end
