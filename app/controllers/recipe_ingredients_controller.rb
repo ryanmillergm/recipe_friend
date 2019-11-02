2e77f996a2c83d06bf29ac1eeab26915fd87ab57
Ryan Miller <ryanmillergm@gmail.com>
2019-11-01 18:19:19 -0600

    Adds recipe ingredient destroy method

---
+++ app/controllers/recipe_ingredients_controller.rb
@@ -0,0 +1,15 @@
+class RecipeIngredientsController < ApplicationController
+
+  def destroy
+    @recipe_ingredient = RecipeIngredient.find(params[:id])
+    @recipe = Recipe.find(params[:format])
+    @recipe_ingredient.destroy
+    @ingredient = @recipe.ingredients.first
+    if
+      redirect_to edit_ingredient_path(@recipe.id)
+    else
+      flash[:success] = "Your ingredient has been removed"
+      redirect_to edit_ingredient_path(@ingredient.id, @recipe.id)
+    end
+  end
+end
