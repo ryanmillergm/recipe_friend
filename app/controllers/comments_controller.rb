e9248a7d8ca9aa2c434889348218a36211674357
Ryan Miller <ryanmillergm@gmail.com>
2019-11-08 13:26:55 -0700

    Adds user_id to permitted params for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,7 +21,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :user_id)
   end
 
   def find_commentable
