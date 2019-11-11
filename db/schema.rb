45af7207d24e3d29c8eacfb4c97d712bcb3e7f66
Ryan Miller <ryanmillergm@gmail.com>
2019-11-11 13:55:31 -0700

    Adds rating column to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_200326) do
+ActiveRecord::Schema.define(version: 2019_11_11_181458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 2019_11_08_200326) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "rating"
+    t.bigint "recipe_id"
+    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -116,6 +119,7 @@ ActiveRecord::Schema.define(version: 2019_11_08_200326) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "recipes"
   add_foreign_key "comments", "users"
   add_foreign_key "favorites", "recipes"
   add_foreign_key "favorites", "users"
