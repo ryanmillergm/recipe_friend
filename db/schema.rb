ef67d826e577e9de6580434160088d3af4ae062f
Ryan Miller <ryanmillergm@gmail.com>
2019-11-12 10:14:33 -0700

    Removes recipe_id reference from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_181458) do
+ActiveRecord::Schema.define(version: 2019_11_11_181152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,10 +42,8 @@ ActiveRecord::Schema.define(version: 2019_11_11_181458) do
     t.string "commentable_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
     t.integer "rating"
-    t.bigint "recipe_id"
-    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -119,7 +117,6 @@ ActiveRecord::Schema.define(version: 2019_11_11_181458) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "recipes"
   add_foreign_key "comments", "users"
   add_foreign_key "favorites", "recipes"
   add_foreign_key "favorites", "users"
