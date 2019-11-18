a0d6591639f785ff8c256cd20f242bdc214bb749
Ryan Miller <ryanmillergm@gmail.com>
2019-11-18 14:20:22 -0700

    Updates user can visit profile spec

--- spec/features/users/user_profile/user_can_visit_profile_spec.rb
+++ spec/features/users/user_profile/user_can_visit_profile_spec.rb
@@ -8,19 +8,16 @@ RSpec.describe "As a user" do
 
   it 'I can visit my profile page' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
-
     # visit root_path
 
     # find("#user-options-path}").find("#my-profile-path").click
 
-    visit dashboard_my_profile_index_path
-    save_and_open_page
+    visit dashboard_my_profile_path(@user1.id)
 
-    expect(page).to have_content(@user1.first_name)
-    expect(page).to have_content(@user1.last_name)
-    expect(page).to have_content("Edit Profile")
+    expect(page).to have_content(@user1.username)
     expect(page).to have_content("About Me")
     expect(page).to have_content("Location")
     expect(page).to have_content("Favorite Quote")
+    expect(page).to have_link("Edit Profile")
   end
 end
