14f0bdc22171d50f8481c108b51cb97ae54b8999
Ryan Miller <ryanmillergm@gmail.com>
2019-11-20 13:28:15 -0700

    Adds crop images controller with new and create methods

---
+++ app/controllers/crop_images_controller.rb
@@ -0,0 +1,28 @@
+class CropImagesController < ApplicationController
+  def new
+    @user = current_user
+  end
+
+  def create
+    @user = current_user
+    if @user.avatar.attached? &&
+        params[:coords_x].present? &&
+        params[:coords_y].present? &&
+        params[:coords_w].present? &&
+        params[:coords_h].present?
+
+        x = params[:coords_x]
+        y = params[:coords_y]
+        w = params[:coords_w]
+        h = params[:coords_h]
+      @user.update(avatar_coords: "#{w}x#{h}+#{x}+#{y}")
+      @user.save
+
+      flash[:success] = "Successfully Saved"
+      redirect_to dashboard_my_profile_path(@user.id)
+    else
+      flash[:notice] = "You must submit cropping dimensions"
+      render :new
+    end
+  end
+end
