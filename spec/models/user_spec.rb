b9a6d5dcb260009cff74a6f88e3dfc3f1353edeb
Ryan Miller <ryanmillergm@gmail.com>
2019-11-26 17:09:15 -0700

    Removes telephone validation for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :last_name }
     it { should validate_uniqueness_of :username }
     it { should validate_uniqueness_of :email }
-    it { should validate_uniqueness_of :telephone }
+    # it { should validate_uniqueness_of :telephone }
   end
 
   describe "relationships" do
