4dfe263c4d40b64b6437ab1e7461ce6e66f0b49a
Ryan Miller <ryanmillergm@gmail.com>
2019-11-27 16:21:55 -0700

    Adds facebook_login to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,29 +5,15 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    if auth
+    if auth && auth[:provider] != "facebook"
       if user = User.find_by(google_uid: auth.uid)
         session[:user_id] = user.id
         redirect_to dashboard_path
       else
-        user = User.new(google_uid: auth.uid)
-        user.first_name = auth.info.first_name
-        user.last_name = auth.info.last_name
-        user.username = auth.info.first_name
-        user.google_token = auth.credentials.token
-        user.email = auth.info.email
-        user.email_confirmed = true
-        user.confirm_token = nil
-        # user.telephone = 5555555555
-        user.password = "password"
-        if user.save
-          session[:user_id] = user.id
-          redirect_to edit_user_path(user)
-        else
-          flash[:danger] = user.errors.full_messages
-          render :new
-        end
+        google_login(auth)
       end
+    elsif auth && auth[:provider] == "facebook"
+      facebook_login(auth)
     else
       user = User.find_by(email: login_params[:email])
       if user&.authenticate(login_params[:password])
@@ -60,6 +46,51 @@ class SessionsController < ApplicationController
     else
       flash[:error] = 'Sorry, User does not exist'
       redirect_to root_url
+    end
+  end
+
+  def facebook_login(auth)
+    if user = User.find_by(uid: auth.uid)
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
+      user = User.new(uid: auth.uid)
+      user.first_name = auth.info.name.split.first
+      user.last_name = auth.info.name.split.last
+      user.username = auth.info.name
+      # user.facebook_token = auth.credentials.token
+      user.email = auth.info.email
+      user.email_confirmed = true
+      user.confirm_token = nil
+      # user.telephone = 5555555555
+      user.password = "password"
+      if user.save
+        session[:user_id] = user.id
+        redirect_to edit_user_path(user)
+      else
+        flash[:danger] = user.errors.full_messages
+        render :new
+      end
+    end
+  end
+
+  def google_login(auth)
+    user = User.new(google_uid: auth.uid)
+    user.first_name = auth.info.first_name
+    user.last_name = auth.info.last_name
+    user.username = auth.info.first_name
+    user.google_token = auth.credentials.token
+    user.email = auth.info.email
+    user.email_confirmed = true
+    user.confirm_token = nil
+    # user.telephone = 5555555555
+    user.password = "password"
+    if user.save
+      session[:user_id] = user.id
+      redirect_to edit_user_path(user)
+    else
+      flash[:danger] = user.errors.full_messages
+      render :new
     end
   end
 end
