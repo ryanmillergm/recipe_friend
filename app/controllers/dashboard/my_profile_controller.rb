6fe2cac1fb32ea3ac04e470ae0623f54b6414ffe
Ryan Miller <ryanmillergm@gmail.com>
2019-12-10 13:24:00 -0700

    Adds before action authenticate user in my profile controller

--- app/controllers/dashboard/my_profile_controller.rb
+++ app/controllers/dashboard/my_profile_controller.rb
@@ -1,9 +1,20 @@
 class Dashboard::MyProfileController < Dashboard::BaseController
+  before_action :authenticate_user
+
   def show
     @user = current_user
   end
 
   def edit
     @user = current_user
+  end
+
+  private
+
+  def authenticate_user
+    if current_user.id != params[:id].to_i
+      flash[:danger] = "Warning! Attempted Breach!"
+      redirect_to forbidden_path(params[:id])
+    end
   end
 end
