019df65d97712e6be97b24d424fb8fbfffc5983b
Ryan Miller <ryanmillergm@gmail.com>
2019-12-11 13:11:42 -0700

    Adds index to friends controller and updates friends create method

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,16 @@
 class FriendsController < ApplicationController
+  def index
+    @user = current_user
+  end
+
   def create
     @user = current_user
+    @uathor = User.find(params[:format])
     friendship = Friend.new(user_id: params[:user_id], friend_id: params[:format])
-    redirect_to
+    if friendship.save
+      redirect_to user_friends_path(@user)
+    else
+      redirect_to user_path(@author)
+    end
   end
 end
