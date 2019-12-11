0b3086e10d2a5515869860a8ac014e60b48e29c4
Ryan Miller <ryanmillergm@gmail.com>
2019-12-11 13:09:45 -0700

    Adds find friend method to user dashboard facade

--- app/facades/user_dashboard_facade.rb
+++ app/facades/user_dashboard_facade.rb
@@ -4,4 +4,8 @@ class UserDashboardFacade
   def initialize(user)
     @user = user
   end
+
+  def find_friend(user, friend)
+    @friend = Friend.where(user_id: user.id).find_by(friend_id: friend.id)
+  end
 end
