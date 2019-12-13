e12c76a01b5aa237e90dee3b100a893bed38dc25
Ryan Miller <ryanmillergm@gmail.com>
2019-12-13 11:36:19 -0700

    Adds pending friend requests to user dashboard facade

--- app/facades/user_dashboard_facade.rb
+++ app/facades/user_dashboard_facade.rb
@@ -8,4 +8,26 @@ class UserDashboardFacade
   def find_friend(user, friend)
     @friend = Friend.where(user_id: user.id).find_by(friend_id: friend.id)
   end
+
+  def pending_friend_requests
+    @friend_requests = Friend.where(friend_id: @user.id ).where(confirmed: false, blocked: false)
+    @friend_requests.map do |friend|
+      User.find(friend.user_id)
+    end
+  end
+
+  def confirmed_friends
+    @friends = []
+
+    group1 = Friend.where(user_id: @user.id).where(confirmed:true)
+
+    if !group1.empty?
+      group1.each do |friend|
+        user = User.find(friend.friend_id)
+        @friends << user
+      end
+    end
+
+    @friends
+  end
 end
