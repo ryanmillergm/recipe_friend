e7fb4a69d4a731fe3a5532d83de2f83c8b89ef3b
Ryan Miller <ryanmillergm@gmail.com>
2019-12-13 11:35:59 -0700

    Adds update method to friends controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,7 @@
 class FriendsController < ApplicationController
   def index
     @user = current_user
+    @user_facade = UserDashboardFacade.new(@user)
   end
 
   def create
@@ -11,6 +12,26 @@ class FriendsController < ApplicationController
       redirect_to user_friends_path(@user)
     else
       redirect_to user_path(@author)
+    end
+  end
+
+  def update
+    @user = current_user
+    @friendship = Friend.where(friend_id: @user.id).find_by(user_id: params[:id])
+    @friend = User.find(@friendship.user_id)
+    if params[:format] == "accept"
+      @friendship.update(confirmed: true)
+      @user.friends << @friend
+      new_friendship = Friend.where(friend_id: @friend.id).find_by(user_id: @user.id)
+      new_friendship.update(confirmed: true)
+      binding.pry
+      redirect_to user_friends_path(@user)
+    elsif params[:format] == "decline"
+      @friendship.destroy
+      redirect_to user_friends_path(@user)
+    else
+      flash[:danger] = "Oops. Something went wrong."
+      redirect_to user_friends_path(@user)
     end
   end
 end
