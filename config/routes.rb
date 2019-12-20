a8bc7d73809ee2d1aef808102064efd6b3c5a53a
Ryan Miller <ryanmillergm@gmail.com>
2019-12-20 09:43:17 -0700

    Comments out route to alexa controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
     end
   end
 
+  # post '/alexa' do
+  #   p request.body.read
+  # end
+
   get 'auth/google_oauth2', to: 'auth#google_oauth'
   get 'auth/google_oauth2/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
