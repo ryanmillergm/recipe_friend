c75d9ae5ee395369199b3aa2757b9ea404b81662
Ryan Miller <ryanmillergm@gmail.com>
2019-12-31 09:43:49 -0700

    Adds index and show for recipes api controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::RecipesController < Api::V1::ApiBaseController
-  
+
+  def index
+    render json: RecipeSerializer.new(Recipe.all)
+  end
+
+  def show
+    render json: RecipeSerializer.new(Recipe.where(title: params[:id].downcase.capitalize))
+  end
+
 end
