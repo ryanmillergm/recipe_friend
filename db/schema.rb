c48582a7d5c0197a26eb96969a3a3c492b698c4d
Ryan Miller <ryanmillergm@gmail.com>
2020-01-11 06:23:39 -0700

    Adds rooms and room_messages and references to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_233023) do
+ActiveRecord::Schema.define(version: 2020_01_11_131845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,23 @@ ActiveRecord::Schema.define(version: 2019_12_10_233023) do
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
+  create_table "room_messages", force: :cascade do |t|
+    t.bigint "room_id"
+    t.bigint "user_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_room_messages_on_room_id"
+    t.index ["user_id"], name: "index_room_messages_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_rooms_on_name", unique: true
+  end
+
   create_table "steps", force: :cascade do |t|
     t.text "step"
     t.bigint "recipe_id"
@@ -134,5 +151,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_233023) do
   add_foreign_key "recipe_ingredients", "ingredients"
   add_foreign_key "recipe_ingredients", "recipes"
   add_foreign_key "recipes", "users"
+  add_foreign_key "room_messages", "rooms"
+  add_foreign_key "room_messages", "users"
   add_foreign_key "steps", "recipes"
 end
