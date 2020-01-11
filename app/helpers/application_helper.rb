8aa4ba29df8172efba4529b29dbdfba0b16cdd6e
Ryan Miller <ryanmillergm@gmail.com>
2020-01-11 07:53:07 -0700

    Adds gravatar_url helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def gravatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    url = "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
+
   def measurement_type
     [
       ['volume'],
